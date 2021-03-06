f2beffd2d677e95b1b8f75a7e73ded6b6080612b
Michael Dressner <mdressne@terpmail.umd.edu>
2021-03-06 10:51:13 -0500

    Changed first tab to MapActivity

--- app/src/main/java/com/parkanywhereonline/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/parkanywhereonline/ui/main/SectionsPagerAdapter.java
@@ -29,7 +29,12 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        switch (position) {
+            case 0:
+                return new MapsFragment();
+            default:
+                return PlaceholderFragment.newInstance(position + 1);
+        }
     }
 
     @Nullable
